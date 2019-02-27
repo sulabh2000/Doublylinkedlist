c883459db1731d5783f2acb0a61fc1880de19115
sulabh2000 <bansalsulabh13@gmail.com>
2019-02-27 10:43:20 +0530

    init commit

---
+++ src/main/DoublyLinkedList.java
@@ -0,0 +1,25 @@
+package main;
+
+public class DoublyLinkedList {
+    private static class Node
+    {
+        private int data;
+        private Node pre;
+        private Node next;
+        public Node(int data,Node pre,Node next)
+        {
+            this.data=data;
+            this.pre=pre;
+            this.next=next;
+        }
+        public Node(int data)
+        {
+            this.data=data;
+            this.pre=null;
+            this.next=null;
+        }
+
+
+    }
+
+}
