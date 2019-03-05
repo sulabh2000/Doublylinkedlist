cb4b4c312b0348d8ce60fea9f541e5bea8fc5039
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-05 14:40:08 +0530

    add display method

--- src/main/DoublyLinkedList.java
+++ src/main/DoublyLinkedList.java
@@ -84,5 +84,17 @@ public class DoublyLinkedList {
             }
 
         }
+        void display()
+    {
+        System.out.println("Doubly linkedlist");
+        Node temp = head;
+        System.out.println("null<--->");
+        while(temp!=null)
+        {
+            System.out.println(temp.data + "<--->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
 
 }
